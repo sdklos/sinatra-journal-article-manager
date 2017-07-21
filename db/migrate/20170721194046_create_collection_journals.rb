9122a680159c1358521e1a2cd50528a82997c14c
Susanna Klosko <sdklos@gmail.com>
2017-07-21 20:34:57 +0000

    remove join table migration

--- db/migrate/20170721194046_create_collection_journals.rb
+++
@@ -1,8 +0,0 @@
-class CreateCollectionJournals < ActiveRecord::Migration[5.0]
-  def change
-    create_table(:collection_journals) do |t|
-      t.integer :collection_id
-      t.integer :journal_id
-    end
-  end
-end
