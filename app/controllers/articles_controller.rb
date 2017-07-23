61451ccbe75aa1418444909c0aa941c58a5e3324
Susanna Klosko <sdklos@gmail.com>
2017-07-23 01:49:23 +0000

    new article in progress

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,11 +3,21 @@ class ArticlesController < ApplicationController
   get "/articles/new" do
     erb :"/articles/new"
   end
+
+  post "/articles" do
+    @article = Article.create(name: params[:article_name], year: params[:article_year])
+
+  end
   get "/articles/:slug" do
     @article = Article.find_by_slug(params[:slug])
 
     erb :"/articles/show"
+  end
 
+  get "/articles/:slug/edit" do
+    @article = Article.find_by_slug(params[:slug])
+
+    erb :"/articles/edit"
   end
 
 end
