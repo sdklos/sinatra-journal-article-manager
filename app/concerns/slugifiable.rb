d24be61051bda5442000250e3f9ba954b6a666c8
Susanna Klosko <sdklos@gmail.com>
2017-07-24 21:47:43 +0000

    created new article

--- app/concerns/slugifiable.rb
+++ app/concerns/slugifiable.rb
@@ -1,13 +1,13 @@
 module Slugifiable
   module InstanceMethods
     def slug
-      self.name.downcase.gsub("/[\s:]/", "-")
+      self.name.downcase.gsub("/[\s]/", "-")
     end
   end
 
   module ClassMethods
     def find_by_slug(slug)
-      @@name = "#{slug}".gsub("-", "/[\s:]/")
+      @@name = "#{slug}".gsub("-", "/[\s]/")
       self.all.detect {|var| var.name.downcase == @@name}
     end
   end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,17 +5,40 @@ class ArticlesController < ApplicationController
   end
 
   post "/articles" do
-    @article = Article.create(name: params[:article_name], year: params[:article_year])
+    @article = Article.create(name: params["article"]["name"], year: params["article"]["year"])
+    @article.collection_id = current_user.id
+    if !params["author"]["author_first_name"].empty? && !params["author"]["author_last_name"].empty?
+      @article.build_author(first_name: params["author"]["author_first_name"], last_name: params["author"]["author_last_name"])
+    else
+      @article.author_id = params["article"]["author_id"]
+    end
+    @article.save
+    if !params["journal_name"].empty?
+      @article.build_journal(name: params["journal_name"])
+    else
+      @article.journal_id = params["article"]["journal_id"]
+    end
+    @article.details = params["details"]
+    binding.pry
+    if @article.author && @article.journal
+      @article.save
+      redirect("/collections/#{current_user.slug}")
+    else
+      flash[:message] = "Please provide an author and journal for your article."
+      redirect("/articles/new")
+    end
+
+
 
   end
-  get "/articles/:slug" do
-    @article = Article.find_by_slug(params[:slug])
+  get "/articles/:id" do
+    @article = Article.find(params[:id])
 
     erb :"/articles/show"
   end
 
-  get "/articles/:slug/edit" do
-    @article = Article.find_by_slug(params[:slug])
+  get "/articles/:id/edit" do
+    @article = Article.find(params[:id])
 
     erb :"/articles/edit"
   end
